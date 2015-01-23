4e5deb5ab3d9c3b2c791a7ff44dbdb440705ceb8
Miles Fogle <stuntddude@gmail.com>
2015-01-23 13:42:24 -0600

    Imported files

---
+++ ConsoleController.h
@@ -0,0 +1,174 @@
+//Cross-platform console controller class written by Alex Gittemeier and Miles Fogle
+//Handles input, output, text coloring, and waiting/time controls
+//Targets Windows and POSIX systems
+//
+
+#ifndef CONSOLECONTROLLER_H_INCLUDED
+#define CONSOLECONTROLLER_H_INCLUDED
+
+//General includes
+#include <ctime>   //temporal methods
+#include <string>  //input and output
+#include <sstream> //output
+
+#ifdef _WIN32
+//Windows-specific includes
+#include <Windows.h>
+#include <conio.h>
+#include <iostream>
+
+//Color flags to simulate a POSIX-like color implementation
+enum Colors {
+    COLOR_BLACK,  COLOR_RED,     COLOR_GREEN, COLOR_BLUE,
+    COLOR_YELLOW, COLOR_MAGENTA, COLOR_CYAN,  COLOR_WHITE
+};
+
+//special (non-ascii) key codes
+//obviously this is not fully implemented
+enum SpecialKeyCodes {
+    ARROW_LEFT  = 0x00000100,
+    ARROW_RIGHT = 0x00000101,
+    ARROW_DOWN  = 0x00000102,
+    ARROW_UP    = 0x00000103
+};
+
+#else
+
+#include <curses.h>
+
+#endif // _WIN32
+
+
+class ConsoleController {
+    public:
+
+        // Define types
+        typedef unsigned char COLOR_ID;
+        typedef struct COORD_2D {
+            int x, y;
+        } COORD2D, COORD2;
+
+        // Setup and teardown
+        ConsoleController();
+        ~ConsoleController();
+        void initColor(COLOR_ID colorId, int fg, int bg, bool fBold, bool bBold);
+
+        // Accessors
+        COORD_2D getWindowSize();
+        COORD_2D getCurPos();
+
+        // Actions
+        void cls();
+        void moveCursor(int x, int y);
+        void color(COLOR_ID);
+
+        // Output
+        void output(std::string s);
+
+        template <typename TYPE>
+        void output(TYPE t) {
+            std::stringstream ss;
+            ss << t;
+            output(ss.str());
+        }
+
+        template <typename TYPE>
+        void output(COLOR_ID c, TYPE t) {
+            color(c);
+            output(t);
+        }
+
+        template <typename TYPE>
+        void output(int x, int y, TYPE t) {
+            moveCursor(x, y);
+            output(t);
+        }
+
+        template <typename TYPE>
+        void output(int x, int y, COLOR_ID c, TYPE t) {
+            moveCursor(x, y);
+            color(c);
+            output(t);
+        }
+
+        //fix for the commonly distributed GCC bug where std::to_string() doesn't work
+        template <typename TYPE>
+        std::string toString(TYPE t) {
+            std::stringstream ss;
+            ss << t;
+            return ss.str();
+        }
+
+        // Input
+        int getKey();
+        int waitForKey();
+        int waitForNewKey();
+        void waitForKey(int match);
+        int echoKey();
+        int waitForChar();
+
+        std::string waitForInput();
+        std::string waitForInput(char delimiter);
+        std::string waitForInput(std::string delimiter);
+
+        // Type-specific input
+        template <typename TYPE>
+        TYPE waitForInput(std::string delimiters) {
+            std::string str(waitForInput(delimiters));
+            std::stringstream ss;
+            TYPE result;
+
+            ss << str;
+            ss >> result;
+            return result;
+        }
+
+        template <typename TYPE>
+        TYPE waitForInput(char delimiter) {
+            return waitForInput<TYPE>(std::string(1, delimiter));
+        }
+
+        template <typename TYPE>
+        TYPE waitForInput() {
+            return waitForInput<TYPE>("\n");
+        }
+
+        // Temporal methods (consider moving to a different file)
+        void sleepMs(long ms);
+        void throttle(long ms);
+
+        // Utility methods
+        void clearKey();
+        void pause();
+
+    private:
+        // Define types
+        struct COLOR {
+            short foreground, background;
+            bool foreBold, backBold;
+        };
+
+        // Fields
+        clock_t throttleLast;
+        bool throttleInitialized;
+        static int classInstances;
+
+        // Disallow copying and assigning over the object (do not implement these methods)
+        ConsoleController(const ConsoleController &);
+        ConsoleController & operator= (const ConsoleController &);
+
+        //color values made static for compatibility with POSIX implementation
+#ifdef _WIN32
+        // Windows specific fields
+        static HANDLE hStdout;
+        static COLOR colors[256];
+#else
+        // POSIX specific fields
+        static bool foreBoldFlags[256];
+#endif
+};
+
+//declare a static instance available everywhere, similar to std::cout
+static ConsoleController con;
+
+#endif // CONSOLECONTROLLER_H_INCLUDED
